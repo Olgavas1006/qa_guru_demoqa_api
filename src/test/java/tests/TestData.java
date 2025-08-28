a4879a06aab7224a2a6ae73cede14e436c1ad65e
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-08-28 23:30:37 +0500

    added DemoqaBooksTests

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -3,7 +3,7 @@ package tests;
 import models.LoginBodyModel;
 
 public class TestData {
-    public static final String login = System.getProperty("login").toString();
+    public static final String login = System.getProperty("login");
     public static final String password = System.getProperty("password");
 
     public static LoginBodyModel credentials = new LoginBodyModel(login, password);
